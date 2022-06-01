a1aa550774c6a252a2e00c3703ed653a21433d4e
gkeating379 <gtkeating@yahoo.com>
2022-06-01 16:29:15 -0400

    Add comments for which voice code to use

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,11 +1,13 @@
 import pyttsx3
 import praw 
 
+#en_us_010 for male
+#en_au_001 for female
 def make_mp3_from_submission(submission):
     '''takes a submission and creates an mp3 
         reading the title and body of post
     '''
-    
+
     engine = pyttsx3.init()
     engine.save_to_file(submission.title , f'{submission.fullname}_title.mp3')
     engine.save_to_file(submission.selftext , f'{submission.fullname}_body.mp3')
